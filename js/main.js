518e60cb95ff8650237b9731d0c3ced7037389a6
ruzmetovdev <boburruzmetov2006@gmail.com>
2023-02-06 13:04:09 +0500

    jfj

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+
+  window.addEventListener("scroll", function () {
+    shrink();
+  });
+  
+  let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+          navbar.classList.remove("navbar-shrink");
+      }
+  }
+  
